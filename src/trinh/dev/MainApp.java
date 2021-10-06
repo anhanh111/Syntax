48660d47580ed229e22704f8380aae0222ade729
Truong Ngoc Trinh <bachtran1p@gmail.com>
2021-10-06 17:03:32 +0700

    first commit

---
+++ src/trinh/dev/MainApp.java
@@ -0,0 +1,36 @@
+package trinh.dev;
+    /**
+      * @author Admin
+       * ghi chú ( comment nhiều dòng)
+         * 1. Tên lớp trùng file 
+           * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép đầu thì những chữ cái đầu viết hoa
+             * 3. Tên lớp đặt phải có ý nghĩa. Ví dụ: Class peson : lớp mô tả con người 
+               *  - Interface: Phải có tiền tố I. Ví dụ : Ilistener 
+                 *  - Abstract: Phải có tiền tố Abstract. Ví dụ: AbstractShape 
+                   */
+public class MainApp {
+        //comment 1 dòng cách 1. Phím nóng Crtl + /
+		/* comment 1 dòng cách 2 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến:
+		 * 1. Không chứa ký tự đặc biệt !@#$%^&*()_++
+		 * 2. Không chứa ký tự trắng 
+		 * 3. Viết thường 
+		 * 4. Không bắt đầu bằng các chữ số
+		 */
+		int number;
+        String nameSchool;
+	}
+	/**
+	 * Tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+		
+	}
+	
+	
+    }
+
+
